77fd1df324fb4353c053ac6f6c50760a26ef2ea5
Sachin <gautampawar7562@gmail.com>
2024-10-08 21:17:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $('#calendar').evoCalendar({
+
+        
+        theme: "Midnight Blue",
+            calendarEvents: [
+              {
+                id: 'Presentation', // Event's ID (required)
+                name: "Group-A", // Event name (required)
+                description:"Sachin & Yash, Topic-Generative AI",
+                date: "Octobar/9/2024", // Event date (required)
+                type: "holiday", // Event type (required)
+                everyYear: true // Same event every year (optional)
+              },
+              {
+                id: 'Presentation', // Event's ID (required)
+                name: "Group-B", // Event name (required)
+                description:"Sachin & Yash, Topic-Generative AI",
+                date: "Octobar/9/2024", // Event date (required)
+                type: "holiday", // Event type (required)
+                everyYear: true // Same event every year (optional)
+              },
+              {
+                id: 'Presentation', // Event's ID (required)
+                name: "Group-C", // Event name (required)
+                description:"Sachin & Yash,Generative-AI",
+                date: "Octobar/9/2024", // Event date (required)
+                type: "holiday", // Event type (required)
+                everyYear: true // Same event every year (optional)
+              },
+              {
+                id: 'Presentation', // Event's ID (required)
+                name: "Group-D", // Event name (required)
+                description:"Sachin & Yash,Generative-AI",
+                date: "Octobar/10/2024", // Event date (required)
+                type: "holiday", // Event type (required)
+                everyYear: true // Same event every year (optional)
+              },
+              {
+                id: 'Presentation', // Event's ID (required)
+                name: "Group-C", // Event name (required)
+                description:"Sachin & Yash,Generative-AI",
+                date: "Octobar/10/2024", // Event date (required)
+                type: "holiday", // Event type (required)
+                everyYear: true // Same event every year (optional)
+              },
+              {
+                name: "Vacation Leave",
+                badge: "02/13 - 02/15", // Event badge (optional)
+                date: ["February/13/2024", "February/15/2024"], // Date range
+                description: "Vacation leave for 3 days.", // Event description (optional)
+                type: "event",
+                color: "#63d867" // Event custom color (optional)
+              }
+            ]
+    })
+})
